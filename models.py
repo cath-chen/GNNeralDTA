2294974f3992b14a0ed2d3b050816d0e3f24ddb2
JanLohse <jan.lars.lohse@gmail.com>
2024-12-09 13:44:04 +0800

    generalize gnn module

--- models.py
+++ models.py
@@ -3,28 +3,23 @@ from torch_geometric import nn as gnn
 
 
 class GNN(nn.Module):
-    def __init__(self, in_channels, out_channels, hidden_dims, operator=gnn.GCNConv, dropout=0., **kwargs):
+    def __init__(self, in_channels, out_channels, hidden_dims=[], operator=gnn.GCNConv, dropout=0., **kwargs):
         super(GNN, self).__init__()
 
-        self.input_layer = operator(in_channels, hidden_dims[0], **kwargs)
-        self.hidden_layers = nn.ModuleList(
-            [operator(hidden_dims[i], hidden_dims[i + 1], **kwargs) for i in range(len(hidden_dims) - 1)])
-        self.output_layer = operator(hidden_dims[-1], out_channels, **kwargs)
+        layer_dims = [in_channels] + hidden_dims + [out_channels]
+        self.layers = nn.ModuleList([
+            operator(layer_dims[i], layer_dims[i + 1], **kwargs) for i in range(len(layer_dims) - 1)
+        ])
 
         self.dropout = nn.Dropout(dropout)
         self.relu = nn.ReLU()
 
     def forward(self, x, edge_index):
-        y = self.input_layer(x, edge_index)
-        y = self.relu(y)
-        y = self.dropout(y)
+        for layer in self.layers:
+            x = layer(x, edge_index)
+            x = self.relu(x)
+            x = self.dropout(x)
 
-        for layer in self.hidden_layers:
-            y = layer(y, edge_index)
-            y = self.relu(y)
-            y = self.dropout(y)
-
-        y = self.output_layer(y, edge_index)
-        return y
+        return x
 
 # TODO: Transformer encoder, combination model
