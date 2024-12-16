fa9862bade40253bbf4312c11a76263cac0e2f62
JanLohse <jan.lars.lohse@gmail.com>
2024-12-16 17:27:37 +0800

    test both attention

--- train_test.py
+++ train_test.py
@@ -236,7 +236,36 @@ if __name__ == '__main__':
         print(f"test ci score: {ci_score} test mse: {mse_score}")
 
     else:
-        filename = f"train/{time.strftime('%Y%m%d-%H%M%S')}"
+        filename = f"train/{time.strftime('%Y%m%d-%H%M%S')}_linear"
+
+        model = AttentionGNNeral(drug_dim, prot_dim, attention='linear')
+
+        splits = train_loader
+
+        models, ci_scores, mse_scores = [], [], []
+
+        for i, (train_loader, val_loader) in enumerate(splits):
+            model, results = train(model, train_loader, device, epochs=args.epochs, val_loader=val_loader)
+
+            ci_score = results['train_ci']
+            mse_score = results['train_mse']
+
+            test_ci_score, test_mse_score, _, _ = evaluate(model, test_loader, device)
+
+            models.append(model)
+            ci_scores.append(ci_score)
+            mse_scores.append(mse_score)
+
+            append_print(filename + ".txt",
+                         f"split={i + 1} val_ci_score={ci_score:.3f} val_mse_score={mse_score:.3f} {test_ci_score=:.3f} {test_mse_score=:.3f}")
+
+
+        model_dict = best_model.state_dict()
+
+        with open(filename + "_model.pkl", 'wb') as f:
+            pickle.dump(model_dict, f)
+
+        filename = f"train/{time.strftime('%Y%m%d-%H%M%S')}_cross"
 
         model = AttentionGNNeral(drug_dim, prot_dim)
 
